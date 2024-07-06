2e1e4e853fee742e01b02bd1bfa33921d3b8d990
Roshan Pathak <roshan321pathak@gmail.com>
2024-07-06 16:50:20 +0530

    Bug

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,8 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Comp.Layout />}>
    <Route path='' element={<Comp.Home />} /> 
     
-   <Route path='users/login' element={<Comp.Login />} />
- <Route path='signup' element={<Comp.SignUp />} />
+   <Route path='/users/login' element={<Comp.Login />} />
+ <Route path='/signup' element={<Comp.SignUp />} />
    <Route path='invoice/:username' element={<Comp.AllInvoice />} />
    <Route path='add/:invoiceCode' element={<Comp.InvoiceForm />} />
    <Route path='edit/:invoiceCode' element={<Comp.Edit />} />
